3ebabc10c26e06911e333fc7d4dc0e990d93471e
Roman-Sevryuk <sevrukroman352@gmail.com>
2025-05-02 08:54:38 +0300

    add first task

---
+++ Homework2/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ведите ваше имя");
+        string Name = Console.ReadLine();
+        Console.WriteLine("Введите ваш возраст");
+        int Age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите ваш рост");
+        double Height = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Привет," + Name + "!");
+        Console.WriteLine("\tВаш возраст: " + Age + " лет.");
+        Console.WriteLine("\t\tВаш рост: " + Height + " м.");
+    }
+}
