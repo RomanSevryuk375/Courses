0b6a043b898c7c79fa39bd067b90c316fa0cc0e1
Roman-Sevryuk <sevrukroman352@gmail.com>
2025-05-13 17:45:00 +0300

    Add first task

--- Homework4/H4_T1/Program.cs
+++ Homework4/H4_T1/Program.cs
@@ -8,7 +8,6 @@ namespace H4_T1
         {
             const int length = 10;
             var arr = new int[length] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
             foreach (var item in arr)
             {
                 Console.WriteLine(item);
