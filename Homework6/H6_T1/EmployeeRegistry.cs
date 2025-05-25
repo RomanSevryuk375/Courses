372ecf95387337592e0f17a469b2941ec512c152
Roman-Sevryuk <sevrukroman352@gmail.com>
2025-05-25 14:04:29 +0300

    Add homework6

---
+++ Homework6/H6_T1/EmployeeRegistry.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H6_T1
+{
+    public class EmployeeRegistry
+    {
+
+        private static List<EmployeeBase> employees = new List<EmployeeBase>();
+
+        public static void AddEmployee(EmployeeBase employee)
+        {
+            employees.Add(employee);
+        }
+
+        public static void ListAllEmployees()
+        {
+            foreach (EmployeeBase employee in employees)
+            {
+                Console.WriteLine(employee.GetDetails());
+            }
+        }
+
+        public static void FindEmployee(string name)
+        {
+            var flag = true;
+            if (!string.IsNullOrEmpty(name))
+            {
+                name = char.ToUpper(name[0]) + name.Substring(1).ToLower();
+            }
+            else
+            {
+                Console.WriteLine("You entered a blank name");
+            }
+            foreach (EmployeeBase employee in employees)
+            {
+                if (employee.Name == name)
+                {
+                    flag = false;
+                    Console.Write("Your employee " + employee.GetDetails() + "\n");
+                }
+            }
+            if (flag)
+            {
+                Console.WriteLine("There is no employee with this name");
+            }
+
+        }
+    }
+}
---
+++ Homework6/H6_T1/Manager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H6_T1
+{
+    public class Manager : EmployeeBase
+    {
+        private int _salary;
+
+        public string Qualification { get; set; }
+
+        public Project AssignedProject { get; set; }
+
+        public int Salary
+        {
+            get { return _salary; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Salary cannot be negative");
+                    _salary = 0;
+                }
+                else
+                {
+                    _salary = value;
+                }
+            }
+        }
+
+        public Manager() : base()
+        {
+            Qualification = "Unknown";
+            Salary = 0;
+        }
+
+        public Manager(int salary, string qualification, string name, string position) : base(name, position)
+        {
+            Salary = salary;
+            Qualification = qualification;
+            Name = name;
+            Position = position;
+        }
+
+        public Manager(int salary, string qualification, string name, string position, Project project) : this(salary, qualification, name, position)
+        {
+            AssignedProject = project;
+        }
+
+        public override string GetDetails()
+        {
+            return $"Name: {Name} Position: {Position} Sallary: {Salary} Qualification: {Qualification}";
+        }
+
+        public void AssignProject(string projectName, DateTime deadline)
+        {
+            AssignedProject = new Project(projectName, deadline);
+        }
+
+        public class Project
+        {
+            public string ProjectName;
+
+            public DateTime Deadline;
+
+            public Project()
+            {
+               
+            }
+
+            public Project(string projectName, DateTime deadline)
+            {
+                ProjectName = projectName;
+                Deadline = deadline;
+            }
+
+            public void DisplayProjectInfo()
+            {
+                Console.WriteLine($"Project name - {ProjectName}, deadline - {Deadline}");
+            }
+        }
+    }
+}
+
