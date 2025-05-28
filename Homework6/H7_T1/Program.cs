b756d12355f66ef4562401932488da1a29816006
Roman-Sevryuk <sevrukroman352@gmail.com>
2025-05-28 14:34:43 +0300

    Add first task

---
+++ Homework6/H7_T1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace H7_T1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[6];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Write number");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Array.Sort(array, (x, y) => y.CompareTo(x)); 
+            Console.WriteLine(string.Join(", ", array)); 
+        }
+    }
+}
---
+++ Homework6/H7_T1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("H7_T1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("H7_T1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f815abd9-428f-416a-9bf8-8c3ed3cf0005")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
