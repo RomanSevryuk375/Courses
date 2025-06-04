7ca475c5eb43b556b2003c53dcf622f9d8c0eae5
Roman-Sevryuk <sevrukroman352@gmail.com>
2025-06-04 17:40:05 +0300

    Add third task

---
+++ Homework9/H9_T3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace H9_T3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var uniqWords = new Dictionary<string, int>();
+            Console.WriteLine("Enter your text: ");
+            string inputString = Console.ReadLine();
+            Console.Clear();
+            if (!string.IsNullOrWhiteSpace(inputString))
+            {
+                string[] words = inputString.Split(new[] { ' ', ';', '.', ',', ':', '?', '!', '-' }, StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (uniqWords.ContainsKey(words[i].ToLower()))
+                    {
+                        uniqWords[words[i].ToLower()]++;
+                    }
+                    else
+                    {
+                        uniqWords.Add(words[i].ToLower(), 1);
+                    }
+                }
+            }
+
+            Console.WriteLine("Output");
+            Console.WriteLine($"You text is: {inputString}");
+            foreach (var uniq in uniqWords)
+            {
+                Console.WriteLine($"{uniq.Key} - {uniq.Value} times");
+            }
+        }
+    }
+}
