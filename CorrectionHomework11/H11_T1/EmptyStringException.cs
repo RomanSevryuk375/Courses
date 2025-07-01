e8038ed057e5c4d5085ec5d1ab50268b47d12388
Roman-Sevryuk <sevrukroman352@gmail.com>
2025-07-01 14:26:10 +0300

    add task

---
+++ CorrectionHomework11/H11_T1/EmptyStringException.cs
@@ -0,0 +1,10 @@
+﻿namespace H11_T1;
+
+public sealed class EmptyStringException : Exception
+{
+    public EmptyStringException()
+        : base("Аргумент не может быть пустой строкой.") { }
+
+    public EmptyStringException(string paramName)
+        : base($"Аргумент \"{paramName}\" не может быть пустой строкой.") { }
+}
